5420821b216af1746bda3c31a06bd01f196f429a
EduEnti <eduardo.pordomingo@enti.cat>
2025-04-27 15:20:59 +0200

    Update

--- playlist.py
+++ playlist.py
@@ -2,14 +2,16 @@
 
 from bs4 import BeautifulSoup
 import os
+import base64
 
+#Constantes rutas a directorios
 ALBUMS_PATH = "albums/"
 ARTISTS_PATH = "artists/"
 SONGS_PATH = "songs/"
 GENRES_PATH = "genres/"
 COVERS_PATH = "covers/"
 
-
+#Estructuras para loadear información
 albums = []
 artists = []
 songs = []
@@ -23,6 +25,14 @@ version = 0.5
 
 app_title = "Playlist v" + str(version)
 
+
+
+def image_to_base64(image_path):
+    image = open(image_path, "rb")
+
+    return base64.b64encode(image.read()).decode('utf-8')
+
+
 def print_menu():
     os.system("cls" if os.name == "nt" else "clear")
 
@@ -32,26 +42,58 @@ def print_menu():
     
     print("    MENÚ")
 
-
 def show_menu_songs():
     print_menu()
 
-    print("\n", "1. Listar todas las canciones.", "\n", "2. Buscar canción por título", "\n", "0. Volver", "\n")
+    print("\n1. Listar todas las canciones.\n", "2. Buscar canción por título\n", "0. Volver\n")
 
 def show_menu_albums():
-    print_menu()
+    while True:
+        print_menu()
+        print("\n1. Listar todos los álbumes.\n2. Buscar álbum por título.\n0. Volver\n")
 
-    print("\n", "1. Listar todos los álbumes.", "\n", "2. Buscar álbum por título", "\n", "0. Volver", "\n")
+        option = input("Elige una opción: ")
+
+        if option == "0":
+            break
+        elif option == "1":
+            list_all_albums()
+            input("\nPresiona Enter para continuar...")
+        elif option == "2":
+            print("Buscar álbum -pendiente-.")
+        else:
+            print("ERROR 01: La opción elegida no es válida.")
 
 def show_menu_artists():
     print_menu()
 
-    print("\n", "1. Listar todos los artistas.", "\n", "2. Buscar artista por nombre", "\n", "0. Volver", "\n")
+    print("\n1. Listar todos los artistas.\n2. Buscar artista por nombre\n0. Volver\n")
 
 def show_menu_genres():
     print_menu()
 
-    print("\n", "1. Listar todos los géneros.", "\n", "2. Buscar género por nombre", "\n", "0. Volver", "\n")
+    print("\n1. Listar todos los géneros.\n2. Buscar género por nombre\n0. Volver\n")
+
+
+
+
+
+
+def list_all_albums():
+    print("\n    LISTADO DEL ÁLBUMES\n")
+    print(" "* 3, "-" * 20)
+
+    for album in albums:
+        print(f"\nID: {album['id']}")
+        print(f"\nTítulo: {album['title']}")
+        print(f"Artistas: {', '.join([a['name'] for a in album['artists']])}")
+        print(f"Canciones: {len(album['songs'])}")
+        print(" " * 3, "-" * 20)
+
+
+
+
+
 
 
 def open_xml(file_path):
@@ -60,15 +102,34 @@ def open_xml(file_path):
     return BeautifulSoup(file_xml, 'xml')
 
 
-def load_album_file():
-    file_path = ALBUM_PATH+file_name
+def load_album_file(file_name):
+    file_path = ALBUMS_PATH+file_name
 
     album_xml = open_xml(file_path)
 
     album = {
         "id": album_xml.album["id"],
-        "title": album_xml.title.text
+        "title": album_xml.title.text,
+        "cover": None,
+        "artists": [],
+        "songs": []
     }
+
+    if album_xml.cover:
+        cover_path = COVERS_PATH+album_xml.cover["src"]
+        album["cover"] = image_to_base64(cover_path)
+
+    for artist in album_xml.artists.find_all("artist"):
+        album["artists"].append({
+            "id": artist["id"],
+            "name": artist.text
+            })
+
+    for song in album_xml.songs.find_all("song"):
+        album["songs"].append({
+            "id": song["id"],
+            "title": song.text
+            })
 
     return album
 
@@ -82,15 +143,22 @@ def load_album_num(album_num):
    
 
 def load_albums():
-    global ALBUM_PATH
+    global ALBUMS_PATH
     global albums
+
+    albums = []
 
     albums_dir = os.listdir(ALBUMS_PATH)
 
     for album in albums_dir:
-        if not album.endswith(".xml"):
+        if not album.endswith(".xml") and not album.startswith("album_"):
             continue
         albums.append(load_album_file(album))
+
+    albums.sort(key=lambda x: int(x['id']))
+
+
+load_albums()
 
 
 while True:
@@ -125,56 +193,13 @@ while True:
             elif (respuesta == 4):
                 show_menu_genres()
             elif (respuesta == 0):
-                print("Escogiste el numero", respuesta)
-            break
+                break
 
     else:
         print("ERROR 02: Debes introducir un número.")
         error = 2
 
 
-
-load_albums()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#xml_ejemplo = '<personaje><nombre>Jacinto</nombre><edad valor="33" /></personaje>'
-
-#print(xml_ejemplo)
-
-#personaje = BeautifulSoup(xml_ejemplo, 'xml')
-
-#print(personaje.prettify())
-
-#nombre = personaje.nombre
-
-#print(nombre)
-
-#print(nombre.contents)
-
-#print(nombre.text)
-
-
-#song_xml = open("songs/song_1.xml", "r").read()
-
-#song = BeautifulSoup(song_xml, "xml")
-
-#print(song.title.text)
-#print(int(song.duration["seconds"])/60)
-
-#for artist in song.artists.find_all("artist"):
- #   print(artist.text)
 
 
 
